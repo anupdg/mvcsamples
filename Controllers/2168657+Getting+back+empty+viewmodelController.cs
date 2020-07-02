036ee038987a3aca9dce38e0842d60981c6ef70c
Anup Das Gupta <anupdg@outlook.com>
2020-07-02 12:07:52 -0700

    First commit

---
+++ Controllers/2168657+Getting+back+empty+viewmodelController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCSamples.Models;
+
+namespace MVCSamples.Controllers
+{
+    public class _2168657_Getting_back_empty_viewmodelController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductInShopsCheckboxViewModel vm = new ProductInShopsCheckboxViewModel
+            {
+                ShopName = "Test shop"
+            };
+
+            var random = new Random();
+            for (int i =0; i < 20; i++)
+            {
+                vm.Checkboxes!.Add(new CheckboxViewModel
+                {
+                    ProductName = $"Product {random.Next()}",
+                    Barcode = random.Next(),
+                    ProductId = Guid.NewGuid()
+                });
+            }
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssortmentSave(Guid id, ProductInShopsCheckboxViewModel vm)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ProductInShopsCheckboxViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCSamples.Models
+{
+    public class ProductInShopsCheckboxViewModel
+    {
+        public string ShopName { get; set; } = default!;
+
+        public List<CheckboxViewModel>? Checkboxes { get; set; } = new List<CheckboxViewModel>();
+    }
+    public class CheckboxViewModel
+    {
+        public bool IsSelected { get; set; } = true;
+
+        public string ProductName { get; set; } = default!;
+
+        public long Barcode { get; set; }
+
+        public Guid ProductId { get; set; }
+
+    }
+}
