d068e2b8ca61752cc6ba3343311a48ae514c1acb
Anup Das Gupta <anupdg@outlook.com>
2020-07-02 15:58:52 -0700

    ajax call complex data to asp.net core post endpoint

---
+++ Controllers/2168667-How+to+pass+multiple+parameter+to+mvc+actionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCSamples.Models;
+
+namespace MVCSamples.Controllers
+{
+    public class _2168667_How_to_pass_multiple_parameter_to_mvc_actionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Test1([FromBody] Tuner sdata, string status)
+        {
+            return Json(new { success = true, responseText = "Your message successfuly sent!" });
+        }
+    }
+}
---
+++ Models/Tuner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCSamples.Models
+{
+	public class Tuner
+	{
+		public string Tick1 { get; set; }
+		public string Tick2 { get; set; }
+		public List<Sections> Sections { get; set; }
+	}
+
+	public class Sections
+	{
+		public string Section { get; set; }
+		public string LI { get; set; }
+
+	}
+}
