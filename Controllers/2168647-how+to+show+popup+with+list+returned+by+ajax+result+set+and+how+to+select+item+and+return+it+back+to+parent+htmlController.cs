7051933c71a12afa3cf83768b3259f9a2cf36b24
Anup Das Gupta <anupdg@outlook.com>
2020-07-02 14:49:38 -0700

    ajax data fancybox popup select .net mvc

---
+++ Controllers/2168647-how+to+show+popup+with+list+returned+by+ajax+result+set+and+how+to+select+item+and+return+it+back+to+parent+htmlController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using MVCSamples.Models;
+
+namespace MVCSamples.Controllers
+{
+    public class _2168647_how_to_show_popup_with_list_returned_by_ajax_result_set_and_how_to_select_item_and_return_it_back_to_parent_htmlController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public List<UserViewModel> GetUsers() {
+            var users = new List<UserViewModel>();
+            for (int i = 0; i < 20; i++)
+            {
+                users.Add(GetRandomData.GetRandomDataObject<UserViewModel>());
+            }
+            return users;
+        }
+    }
+}
---
+++ Controllers/GetRandomData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCSamples.Controllers
+{
+    public class GetRandomData
+    {
+        public static T GetRandomDataObject<T>()
+        {
+            T data = Activator.CreateInstance<T>();
+            var r = new Random();
+            foreach(var property in  data.GetType().GetProperties())
+            {
+                if (property.Name.ToLower().Contains("id"))
+                {
+                    property.SetValue(data, r.Next().ToString());
+                }
+                else if (property.Name.ToLower().Contains("email"))
+                {
+                    property.SetValue(data, $"mail-{r.Next()}@dummy.in");
+                }
+                else if (property.Name.ToLower().Contains("phone"))
+                {
+                    property.SetValue(data, $"+91-{r.Next()}");
+                }
+                else
+                {
+                    property.SetValue(data, $"{property.Name}{r.Next()}");
+                }
+
+            }
+            return data;
+        }
+    }
+}
